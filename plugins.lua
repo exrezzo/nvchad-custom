80f26177f554b257b1533a198ceb4515b9a92ac9
Ivan Ardillo <ivan.ardillo@gmail.com>
2024-05-25 20:48:16 +0200

    load dapui with dap

--- plugins.lua
+++ plugins.lua
@@ -26,14 +26,41 @@ local plugins = {
     "github/copilot.vim",
     event = "BufEnter",
   },
-  -- 2 configurazioni per il debug di Go.
-  -- I mappings vengono caricati soltanto quando i plugin sono caricati.
+  -- Configurazione per il debug con DAP e la UI
   {
     "mfussenegger/nvim-dap",
+    dependencies = {
+      {
+        "rcarriga/nvim-dap-ui",
+        dependencies = {
+          "nvim-neotest/nvim-nio",
+        },
+        config = function()
+          local dapui = require("dapui")
+          dapui.setup()
+
+          local dap = require("dap")
+
+          dap.listeners.before.attach.dapui_config = function()
+            dapui.open()
+          end
+          dap.listeners.before.launch.dapui_config = function()
+            dapui.open()
+          end
+          dap.listeners.before.event_terminated.dapui_config = function()
+            dapui.close()
+          end
+          dap.listeners.before.event_exited.dapui_config = function()
+            dapui.close()
+          end
+        end,
+      },
+    },
     init = function()
       require("core.utils").load_mappings("dap")
     end,
   },
+  -- Configurazione per il debug di Go
   {
     "leoluz/nvim-dap-go",
     ft = "go",
@@ -41,33 +68,6 @@ local plugins = {
     config = function(_, opts)
       require("dap-go").setup(opts)
       require("core.utils").load_mappings("dap_go")
-    end,
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    dependencies = {
-      "mfussenegger/nvim-dap",
-      "nvim-neotest/nvim-nio",
-      "leoluz/nvim-dap-go",
-    },
-    config = function()
-      local dapui = require("dapui")
-      dapui.setup()
-
-      local dap = require("dap")
-
-      dap.listeners.before.attach.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.launch.dapui_config = function()
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated.dapui_config = function()
-        dapui.close()
-      end
-      dap.listeners.before.event_exited.dapui_config = function()
-        dapui.close()
-      end
     end,
   },
 }
