930629eee573f15fbeb1221c7587e963244f6c4d
Ivan Ardillo <ivan.ardillo@gmail.com>
2024-07-02 17:10:10 +0200

    enable nvdash on startup

--- chadrc.lua
+++ chadrc.lua
@@ -1,7 +1,13 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'ayu_dark' }
+M.ui = {
+  theme = 'ayu_dark',
+  nvdash = {
+    load_on_startup = true,
+  }
+}
+
 M.plugins = "custom.plugins"
 M.mappings = require "custom.mappings"
 
